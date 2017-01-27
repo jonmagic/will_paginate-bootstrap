8156f1c75d0dbba7b119255ebb08c3176aa115cc
Jonathan Hoyt <hoyt@github.com>
2017-01-26 22:05:38 -0800

    Update to bootstrap 4 compatability

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -25,27 +25,23 @@ module BootstrapPagination
     protected
 
     def page_number(page)
-      link_options = @options[:link_options] || {}
-
       if page == current_page
-        tag("li", tag("span", page), class: "active")
+        tag("li", tag("span", page), class: "active page-item")
       else
-        tag("li", link(page, page, link_options.merge(rel: rel_value(page))))
+        tag("li", link(page, page, link_options.merge(rel: rel_value(page))), class: "page-item")
       end
     end
 
     def previous_or_next_page(page, text, classname)
-      link_options = @options[:link_options] || {}
-
       if page
         tag("li", link(text, page, link_options), class: classname)
       else
-        tag("li", tag("span", text), class: "%s disabled" % classname)
+        tag("li", tag("span", text), class: "%s disabled page-item" % classname)
       end
     end
 
     def gap
-      tag("li", tag("span", ELLIPSIS), class: "disabled")
+      tag("li", tag("span", ELLIPSIS), class: "disabled page-item")
     end
 
     def previous_page
@@ -60,6 +56,10 @@ module BootstrapPagination
 
     def ul_class
       ["pagination", @options[:class]].compact.join(" ")
+    end
+
+    def link_options
+      (@options[:link_options] || {}).merge({class: "page-link"})
     end
   end
 end
